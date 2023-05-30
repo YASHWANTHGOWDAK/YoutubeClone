2d5b652366dc39d922801c8e96ba807f35ffd85b
YASHWANTHGOWDAK <yashwanth0097@gmail.com>
2023-05-30 16:21:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Youtube</h1>
+    
+    <Navbar/>
+
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar()
+{
+    return (
+        <div className="navbar">
+
+            <h1>Youtube</h1>
+
+        </div>
+    )
+}
+export default Navbar;
