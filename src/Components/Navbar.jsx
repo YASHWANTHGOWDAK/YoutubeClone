22e18dc105416ae0ebad4ae8c126a5dd2813f133
YASHWANTHGOWDAK <yashwanth0097@gmail.com>
2023-05-31 06:59:31 +0530

    changes updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,29 @@
+import YTLogo from '../Images/YTLogo.png'
+import '../Styles/navbar.css'
+import notification from '../Images/notification.png'
+import create from '../Images/create.png'
+
+
 function Navbar()
 {
     return (
         <div className="navbar">
 
-            <h1>Youtube</h1>
+            <div className="logo">
+                <img width={150} src={YTLogo} alt="" />
+            </div>
+            <div className="searchBar">
+                <input type="text" placeholder="search" />
+
+            </div>
+            <div className="userOption">
+            <img width={35} src={create} alt="" />
+            <img width={35} src={notification} alt="" />
+              
+              <div className="profile">
+                <h3>Y</h3>
+              </div>
+            </div>
 
         </div>
     )
